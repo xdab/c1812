ad9500243e47888bd7116debfdde60bfd58f4320
PW <przemyslawxdab@gmail.com>
2023-10-30 15:45:56 +0100

    Parameter input and validation

--- include/c1812.h
+++ include/c1812.h
@@ -1,6 +1,75 @@
 #ifndef C1812_H
 #define C1812_H
 
-int test(int a, int b);
+typedef enum {
+	VERTICAL = 1,
+	HORIZONTAL = 2,
+} c1812_polarization_t;
+
+typedef enum {
+	COASTAL_LAND = 1,
+	INLAND = 3,
+	SEA = 4,
+} c1812_radioclimactic_zone_t;
+
+typedef struct {
+	// Scalar data
+	double f; // frequency [GHz]
+	double p; // percentage of average year for which the calculated signal level is exceeded [%]
+	double htg; // transmitter height above ground level [m]
+	double hrg; // receiver height above ground level [m]
+	c1812_polarization_t pol;
+	c1812_radioclimactic_zone_t zone; 
+	double ws; // street width [m]
+
+	// Vector data
+	int n; // number of points
+	double* d; // distance from transmitter [km]
+	double* h; // terrain height above sea level [m]
+	double* Ct; // representative clutter height [m] 
+
+} c1812_parameters_t;
+
+typedef enum {
+	C1812_NULL = -1,
+	C1812_OK = 0,
+	C1812_INVALID_FREQUENCY = 1,
+	C1812_INVALID_PERCENTAGE_OF_TIME = 2,
+	C1812_INVALID_TRANSMITTER_HEIGHT = 3,
+	C1812_INVALID_RECEIVER_HEIGHT = 4,
+	C1812_INVALID_POLARIZATION = 5,
+	C1812_INVALID_RADIOCLIMACTIC_ZONE = 6,
+	C1812_INVALID_STREET_WIDTH = 7,
+	C1812_INVALID_NUMBER_OF_POINTS = 8,
+	C1812_INVALID_DISTANCE_FROM_TRANSMITTER = 9,
+	C1812_INVALID_TERRAIN_HEIGHT = 10,
+	C1812_INVALID_CLUTTER_HEIGHT = 11,
+} c1812_error_t;
+
+const double C1812_MIN_FREQUENCY = 0.03;
+const double C1812_MAX_FREQUENCY = 6.0;
+
+const double C1812_MIN_TIME_PERCENTAGE = 1.0;
+const double C1812_MAX_TIME_PERCENTAGE = 50.0;
+
+const double C1812_MIN_TRANSMITTER_HEIGHT = 1.0;
+const double C1812_MAX_TRANSMITTER_HEIGHT = 3000.0;
+
+const double C1812_MIN_RECEIVER_HEIGHT = 1.0;
+const double C1812_MAX_RECEIVER_HEIGHT = 3000.0;
+
+const double C1812_MIN_STREET_WIDTH = 1.0;
+const double C1812_DEFAULT_STREET_WIDTH = 27.0;
+const double C1812_MAX_STREET_WIDTH = 100.0;
+
+const int C1812_MIN_NUMBER_OF_POINTS = 2;
+
+const double C1812_MIN_TERRAIN_HEIGHT = -500.0; // Less than the lowest point on Earth
+const double C1812_MAX_TERRAIN_HEIGHT = 9000.0; // More than the highest point on Earth
+
+const double C1812_MIN_CLUTTER_HEIGHT = 0.0; // No clutter
+const double C1812_MAX_CLUTTER_HEIGHT = 1000.0; // More than the highest building in the world
+
+c1812_error_t c1812_validate(c1812_parameters_t *parameters);
 
 #endif 
--- src/c1812.c
+++ src/c1812.c
@@ -1,6 +1,80 @@
-#include "c1812.h"
+#include "../include/c1812.h"
+#include <stdlib.h>
 
-int test(int a, int b) {
-	return a + b;
+c1812_error_t c1812_validate_scalar_data(c1812_parameters_t *parameters) {
+	if (parameters->f < C1812_MIN_FREQUENCY || parameters->f > C1812_MAX_FREQUENCY) {
+		return C1812_INVALID_FREQUENCY;
+	}
+	if (parameters->p < C1812_MIN_TIME_PERCENTAGE || parameters->p > C1812_MAX_TIME_PERCENTAGE) {
+		return C1812_INVALID_PERCENTAGE_OF_TIME;
+	}
+	if (parameters->htg < C1812_MIN_TRANSMITTER_HEIGHT || parameters->htg > C1812_MAX_TRANSMITTER_HEIGHT) {
+		return C1812_INVALID_TRANSMITTER_HEIGHT;
+	}
+	if (parameters->hrg < C1812_MIN_RECEIVER_HEIGHT || parameters->hrg > C1812_MAX_RECEIVER_HEIGHT) {
+		return C1812_INVALID_RECEIVER_HEIGHT;
+	}
+	if (parameters->pol != VERTICAL && parameters->pol != HORIZONTAL) {
+		return C1812_INVALID_POLARIZATION;
+	}
+	if (parameters->zone != COASTAL_LAND && parameters->zone != INLAND && parameters->zone != SEA) {
+		return C1812_INVALID_RADIOCLIMACTIC_ZONE;
+	}
+	if (parameters->ws < C1812_MIN_STREET_WIDTH || parameters->ws > C1812_MAX_STREET_WIDTH) {
+		return C1812_INVALID_STREET_WIDTH;
+	}
+	return C1812_OK;
+}
+
+c1812_error_t c1812_validate_vector_data(c1812_parameters_t *parameters) 
+{
+	if (parameters->n < C1812_MIN_NUMBER_OF_POINTS) {
+		return C1812_INVALID_NUMBER_OF_POINTS;
+	}
+	if (parameters->d == NULL) {
+		return C1812_INVALID_DISTANCE_FROM_TRANSMITTER;
+	}
+	// Validate distance from transmitter is non-negative and increasing
+	for (int i = 0; i < parameters->n; i++) {
+		if (parameters->d[i] < 0) {
+			return C1812_INVALID_DISTANCE_FROM_TRANSMITTER;
+		}
+		if (i > 0 && parameters->d[i] <= parameters->d[i-1]) {
+			return C1812_INVALID_DISTANCE_FROM_TRANSMITTER;
+		}
+	}
+	if (parameters->h == NULL) {
+		return C1812_INVALID_TERRAIN_HEIGHT;
+	}
+	// Validate terrain height is between min/max bounds
+	for (int i = 0; i < parameters->n; i++) {
+		if (parameters->h[i] < C1812_MIN_TERRAIN_HEIGHT || parameters->h[i] > C1812_MAX_TERRAIN_HEIGHT) {
+			return C1812_INVALID_TERRAIN_HEIGHT;
+		}
+	}
+	// If clutter height data is present, validate it
+	if (parameters->Ct != NULL) {
+		for (int i = 0; i < parameters->n; i++) {
+			if (parameters->Ct[i] < C1812_MIN_CLUTTER_HEIGHT || parameters->Ct[i] > C1812_MAX_CLUTTER_HEIGHT) {
+				return C1812_INVALID_CLUTTER_HEIGHT;
+			}
+		}
+	}
+}
+
+c1812_error_t c1812_validate(c1812_parameters_t *parameters)
+{
+	if (parameters == NULL) {
+		return C1812_NULL;
+	}
+	c1812_error_t error = c1812_validate_scalar_data(parameters);
+	if (error != C1812_OK) {
+		return error;
+	}
+	error = c1812_validate_vector_data(parameters);
+	if (error != C1812_OK) {
+		return error;
+	}
+	return C1812_OK;
 }
 
